d7f1bcd6d65bad312767e3c76bc5a9485ea7dd56
Pitro <Pitro@pit.ro>
2020-04-12 19:02:56 +0300

    Контроллер WebApi для работы с сотрудниками

---
+++ Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesApiController : ControllerBase, IEmployeesData
+    {
+        private readonly IEmployeesData employeesData;
+
+        public EmployeesApiController(IEmployeesData employeesData) => this.employeesData = employeesData;
+
+        [HttpPost]
+        public void Add([FromBody] Employee employee) => employeesData.Add(employee);
+
+        [HttpDelete("{id}")]
+        public bool Delete(int id) => employeesData.Delete(id);
+
+        [HttpPut("{id}")]
+        public void Edit(int id, [FromBody] Employee employee) => employeesData.Edit(id, employee);
+
+        [HttpGet]
+        public IEnumerable<Employee> GetAll() => employeesData.GetAll();
+
+        [HttpGet("{id}")]
+        public Employee GetById(int id) => employeesData.GetById(id);
+
+        public void SaveChanges() => employeesData.SaveChanges();
+    }
+}
