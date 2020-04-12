b4f26970a6435ccf7946aaeeb49307b76a8a1025
Pitro <Pitro@pit.ro>
2020-04-12 22:53:17 +0300

    DTO модели для передачи данных по товару

---
+++ Common/WebStore.Domain/DTO/Products/BrandDTO.cs
@@ -0,0 +1,10 @@
+﻿using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.DTO.Products
+{
+    public class BrandDTO : INamedEntity
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ Common/WebStore.Domain/DTO/Products/SectionDTO.cs
@@ -0,0 +1,10 @@
+﻿using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.DTO.Products
+{
+    public class SectionDTO : INamedEntity
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
