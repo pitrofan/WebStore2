fbd5ae3cf857207e7357d277bd2bb3767c33aec4
Pitro <Pitro@pit.ro>
2020-04-13 16:15:26 +0300

    Клиент WebApi каталога товаров

---
+++ Services/WebStore.Clients/Products/ProductsClient.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using WebStore.Clients.Base;
+using WebStore.Domain;
+using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Clients.Products
+{
+    public class ProductsClient : BaseClient, IProductData
+    {
+        public ProductsClient(IConfiguration configuration) : base(configuration, WebApi.Products)
+        {
+        }
+
+        public IEnumerable<Brand> GetBrands() => Get<List<Brand>>($"{serviceAddress}/brands");
+
+        public ProductDTO GetProductById(int id) => Get<ProductDTO>($"{serviceAddress}/{id}");
+
+        public IEnumerable<ProductDTO> GetProducts(ProductFilter Filters = null) =>
+            Post(serviceAddress, Filters)
+            .Content
+            .ReadAsAsync<List<ProductDTO>>()
+            .Result;
+
+        public IEnumerable<Section> GetSections() => Get<List<Section>>($"{serviceAddress}/sections");
+    }
+}
