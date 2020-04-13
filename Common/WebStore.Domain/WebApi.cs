a47e2943e8652bdf8b28a4d08c8da49908d1fae8
Pitro <Pitro@pit.ro>
2020-04-13 14:20:08 +0300

    Контроллер WebApi каталога товаров

--- Common/WebStore.Domain/WebApi.cs
+++ Common/WebStore.Domain/WebApi.cs
@@ -7,5 +7,6 @@ namespace WebStore.Domain
     public static class WebApi
     {
         public const string Employees = "api/employees"; 
+        public const string Products = "api/products"; 
     }
 }
