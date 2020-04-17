6c842a5020baf75f5e64178b8186ce121f4fce93
Pitro <Pitro@pit.ro>
2020-04-17 22:35:20 +0300

    Конфигурированиебиблиотеки Serilog

--- UI/WebStore/Program.cs
+++ UI/WebStore/Program.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Console;
 using Microsoft.Extensions.Options;
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Json;
 
 namespace WebStore
 {
@@ -33,8 +36,15 @@ namespace WebStore
 						//log.AddEventLog();
 						//log.AddFilter("WebStore.Controllers.AccountController", LogLevel.Warning);
 						//log.AddFilter<ConsoleLoggerProvider>((category, level) => category.StartsWith("WebStore") && level > LogLevel.Warning);
-
-					});
+					})
+					.UseSerilog((host, log) => log.ReadFrom.Configuration(host.Configuration)
+						.MinimumLevel.Debug()
+						.MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+						.Enrich.FromLogContext()
+						.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss.fff} {Level:u3}]{SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}")
+						.WriteTo.RollingFile($@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log")
+						.WriteTo.File(new JsonFormatter(",", true), $@".\Logs\WebStore[{DateTime.Now:yyyy-MM-ddTHH-mm-ss}].log.json")
+					);
 				});
 	}
 }
