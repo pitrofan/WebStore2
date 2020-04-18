94ee04155c5bf252b8cb8b9b3f1e9638a2693603
Pitro <Pitro@pit.ro>
2020-04-18 20:07:07 +0300

    Создан проект для модульного тестирования WebStore

--- UI/WebStore/Controllers/HomeController.cs
+++ UI/WebStore/Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace WebStore.Controllers
 
         //public IActionResult Cart() => View(); // была корзина в Views -> Home -> Cart.cshtml
 
-        public IActionResult CheckOut() => View();
+        //public IActionResult CheckOut() => View();
 
         public IActionResult ContactUs() => View();
     }
