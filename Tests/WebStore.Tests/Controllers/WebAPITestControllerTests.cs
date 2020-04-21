43d2afec92fc27f9532fab96afbfe0c5ddab39bc
Pitro <Pitro@pit.ro>
2020-04-21 13:16:19 +0300

    Пример работы библиотеки Moq - Создаем тест использующий макет сервиса (стабл)

---
+++ Tests/WebStore.Tests/Controllers/WebAPITestControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Controllers;
+using WebStore.Interfaces.Api;
+
+using Assert = Xunit.Assert;
+
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class WebAPITestControllerTests
+    {
+        [TestMethod]
+        public async Task Index_Retutns_View_Values()
+        {
+            var expectedResult = new[] { "1", "2", "3" };
+
+            var valueServiceMock = new Mock<IValuesService>();
+
+            valueServiceMock
+                .Setup(x => x.GetAsync())
+                .ReturnsAsync(expectedResult);
+
+            var controller = new WebAPITestController(valueServiceMock.Object);
+
+            var result = await controller.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<string>>(viewResult.Model);
+
+            Assert.Equal(expectedResult.Length, model.Count());
+        }
+    }
+}
