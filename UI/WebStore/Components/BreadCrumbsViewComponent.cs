c3291eaf24f810f83615f3871ed3f3fdc6178906
Pitro <Pitro@pit.ro>
2020-04-23 21:01:14 +0300

    Представление компонента хлебных крошек

--- UI/WebStore/Components/BreadCrumbsViewComponent.cs
+++ UI/WebStore/Components/BreadCrumbsViewComponent.cs
@@ -3,11 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Components
 {
     public class BreadCrumbsViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
+        private readonly IProductData _ProductData;
+
+        public BreadCrumbsViewComponent(IProductData ProductData) => _ProductData = ProductData;
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
     }
 }
