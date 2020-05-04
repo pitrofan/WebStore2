8459bb1697261cfc0483188ab969e3049a371745
Pitro <Pitro@pit.ro>
2020-05-04 21:01:13 +0300

    Доработка методов скрипта декремента и удаления товаров из корзины

--- UI/WebStore/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -55,6 +55,42 @@
         var button = $(this);
         const id = button.data("id"); // data-id="..."
 
+        var container = button.closest("tr");
+
+        $.get(Cart._properties.addToCartLink + "/" + id)
+            .done(function () {
+                const count = parseInt($(".cart_quantity_input", container).val());
+                $(".cart_quantity_input", container).val(count + 1);
+
+                Cart.refreshPrice(container);
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("incrementItem fail"); });
+    },
+
+    refreshPrice: function (container) {
+        const quantity = parseInt($(".cart_quantity_input", container).val());
+        const price = parseFloat($(".cart_price", container).data("price"));
+
+        const totalPrice = quantity * price;
+        const value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+
+        $(".cart_total_price", container).data("price", totalPrice);
+        $(".cart_total_price", container).html(value);
+
+        Cart.refreshTotalPrice();
+    },
+
+    refreshTotalPrice: function () {
+        var total = 0;
+
+        $(".cart_total_price").each(function () {
+            const price = parseFloat($(this).data("price"));
+            total += price;
+        });
+
+        const value = total.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+        $("#total-order-sum").html(value);
     },
 
     decrementItem: function (event) {
@@ -63,6 +99,22 @@
         var button = $(this);
         const id = button.data("id"); // data-id="..."
 
+        var container = button.closest("tr");
+
+        $.get(Cart._properties.decrementLink + "/" + id)
+            .done(function () {
+                const count = parseInt($(".cart_quantity_input", container).val());
+                if (count > 1) {
+                    $(".cart_quantity_input", container).val(count - 1);
+                    Cart.refreshPrice(container);
+                    Cart.refreshCartView();
+                } else {
+                    container.remove();
+                    Cart.refreshTotalPrice();
+                    Cart.refreshCartView();
+                }
+            })
+            .fail(function () { console.log("decrementItem fail"); });
     },
 
     removeFromCart: function (event) {
@@ -71,5 +123,12 @@
         var button = $(this);
         const id = button.data("id"); // data-id="..."
 
+        $.get(Cart._properties.removeFromCartLink + "/" + id)
+            .done(function () {
+                button.closest("tr").remove();
+                Cart.refreshTotalPrice();
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("removeFromCart fail"); });
     }
 } 
